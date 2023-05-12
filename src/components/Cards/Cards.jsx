b34549746f606260e1dcb4272883279d16a0edb0
Sir0s <maty.solis87@gmail.com>
2023-05-12 18:44:00 -0300

    bug fixed

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,6 +3,7 @@ import Card from '../Card/Card';
 import styles from './Cards.module.css'
 export default function Cards({characters,onClose}) {
    return (
+      
    <div className={styles.cards}>{
       characters.map((props) =>
       {
@@ -16,7 +17,7 @@ export default function Cards({characters,onClose}) {
          gender={props.gender}
          origin={props.origin.name}
          image={props.image}
-         onClose={props.onClose}>
+         onClose={onClose}>
          {props.name}
          
          </Card>
